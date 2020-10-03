a729679e8c4851ab826aa8fae1d5d53aa8299e37
ofried301 <jobr2develop@gmail.com>
2020-10-03 18:26:53 +0200

    First GitHub commit message for this project

---
+++ app/src/main/java/de/breitmeier/android/gitremotetest/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.breitmeier.android.gitremotetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
